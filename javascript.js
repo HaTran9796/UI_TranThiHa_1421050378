670ed48789bc980c8446e2df7fd9dca559778719
HaTran <ha.tran@amela.vn>
2020-04-17 21:33:32 +0700

    html/17_4

--- javascript.js
+++ javascript.js
@@ -1,78 +1,96 @@
 
 // <!-- banner-swipper -->
 var bannerSwiper = new Swiper ('.group--banner-swiper', {
-        centeredSlides: true,
-        autoplay: {
-        delay: 7200,
-        disableOnInteraction: false,
-        },
-        pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        },
+  centeredSlides: true,
+  autoplay: {
+  delay: 7200,
+  disableOnInteraction: false,
+  },
+  pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+  },
 })
 
 // attractive-swiper
 var swiper = new Swiper('.attractive-swiper', {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        // loop: true,
-        // autoplay: {
-        //     delay: 2200,
-        //     disableOnInteraction: false,
-        // },
-        navigation: {
-        nextEl: '.review-button-next',
-        prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-        768: {
-        slidesPerView: 2,
-        spaceBetween: 20
-        },
-        991:{
-        slidesPerView: 3,
-        spaceBetween: 30
-        },
-        1024: {
-        slidesPerView: 4,
-        spaceBetween: 40
-        }
-        }
+  slidesPerView: 4,
+  spaceBetween: 30,
+  // loop: true,
+  // autoplay: {
+  //     delay: 2200,
+  //     disableOnInteraction: false,
+  // },
+  navigation: {
+    nextEl: '.review-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    991:{
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
 });
 
 // customerReviews-swipper
 var swiper = new Swiper('.customerReviews-swipper', {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        loop: true,
-        autoplay: {
-            delay: 7200,
-            disableOnInteraction: false,
-        }
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 7200,
+    disableOnInteraction: false,
+  }
 });
 
 // attractive-swiper
 var swiper = new Swiper('.seasonal-swiper', {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        loop: true,
-        breakpoints: {
-        768: {
-        slidesPerView: 2,
-        spaceBetween: 20
-        },
-        991:{
-        slidesPerView: 3,
-        spaceBetween: 30
-        },
-        1024: {
-        slidesPerView: 3,
-        spaceBetween: 40
-        }
-        }
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    991:{
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
 });
 
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
+});
 
 //Get the button
 var mybutton = document.getElementById("myBtn");
@@ -96,12 +114,6 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-// 
-$(function () {
-  $('.hidden').click(function() {
-      $('#dangnhap').modal('hide')
-  })
-})
 
 // icon feather
 feather.replace()
